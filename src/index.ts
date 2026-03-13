4be0e35512e0505195602f6483047c859995a69e
Le-Thi-Truc-Ha <159922862+Le-Thi-Truc-Ha@users.noreply.github.com>
2026-03-13 16:53:55 +0700

    Edit port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 
 dotenv.config();
 
-const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const port: number = process.env.PORT || 3000;
 const app: Application = express();
 
 configCors(app);
