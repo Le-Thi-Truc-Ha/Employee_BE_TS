e42925e9a5886be31724c83a241bd906cc1727a0
Le-Thi-Truc-Ha <hale071204@gmail.com>
2026-04-08 19:50:48 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 const app: Application = express();
 
 configCors(app);
@@ -30,6 +30,7 @@ cron.schedule("0 8 * * *", async () => {
     timezone: "Asia/Ho_Chi_Minh"
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
